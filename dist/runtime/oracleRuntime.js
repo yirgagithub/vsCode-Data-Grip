81dd9bdce48528b2e113f639d1a01960416e12df
Yirga <yirga.hagos@avow.tech>
2026-06-24 23:58:53 +0200

    build: regenerate dist runtimes after merge

--- dist/runtime/oracleRuntime.js
+++ dist/runtime/oracleRuntime.js
@@ -1,4 +1,6 @@
 "use strict";
-const oracledb = require("oracledb");
+
+// src/runtime/oracleRuntime.ts
+var oracledb = require("oracledb");
 module.exports = oracledb;
 //# sourceMappingURL=oracleRuntime.js.map
