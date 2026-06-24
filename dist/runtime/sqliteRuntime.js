1f9c29fbd4f029789794d98c5cbcc28c60cfadd5
Yirga <yirga.hagos@avow.tech>
2026-06-24 23:58:53 +0200

    build: regenerate dist runtimes after merge

--- dist/runtime/sqliteRuntime.js
+++ dist/runtime/sqliteRuntime.js
@@ -1,4 +1,6 @@
 "use strict";
-const sqlite3 = require("sqlite3");
+
+// src/runtime/sqliteRuntime.ts
+var sqlite3 = require("sqlite3");
 module.exports = sqlite3;
 //# sourceMappingURL=sqliteRuntime.js.map
