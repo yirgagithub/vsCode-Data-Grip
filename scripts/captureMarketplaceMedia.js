a3f2edbbb8ce81faa9ab65edce36d2bd8ee3aff7
Yirga Hagos <yirga.hagos@avow.tech>
2026-07-17 16:22:01 +0200

    fix: enable marketplace seed command in development

--- scripts/captureMarketplaceMedia.js
+++ scripts/captureMarketplaceMedia.js
@@ -2,8 +2,7 @@ const { spawnSync } = require('node:child_process');
 
 run('npm', ['run', 'build']);
 run('npm', ['run', 'compile:e2e']);
-run('npx', ['vscode-test', '--code-version', '1.128.1'], {
-  QUERYDECK_ENABLE_TEST_COMMANDS: 'true',
+run('npx', ['vscode-test'], {
   MARKETPLACE_MEDIA_CAPTURE_DIR: 'media/marketplace'
 });
 
