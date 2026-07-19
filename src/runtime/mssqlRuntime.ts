78a90ba7dd48af75ec07ef8ee557a8fd24c87053
Yirga Hagos <yirga.hagos@avow.tech>
2026-07-19 12:37:40 +0200

    docs: add QueryDeck interview study guide

--- src/runtime/mssqlRuntime.ts
+++ src/runtime/mssqlRuntime.ts
@@ -1,1 +1,22 @@
-export { ConnectionPool } from 'mssql';
+import * as mssql from 'mssql';
+
+type MssqlTemporalRuntime = {
+  valueHandler: Map<unknown, (value: Date | null) => unknown>;
+  Date: unknown;
+  Time: unknown;
+  DateTime: unknown;
+  DateTime2: unknown;
+  SmallDateTime: unknown;
+  DateTimeOffset: unknown;
+};
+
+const temporalRuntime = mssql as unknown as MssqlTemporalRuntime;
+
+export const ConnectionPool = mssql.ConnectionPool;
+export const valueHandler = temporalRuntime.valueHandler;
+export const Date = temporalRuntime.Date;
+export const Time = temporalRuntime.Time;
+export const DateTime = temporalRuntime.DateTime;
+export const DateTime2 = temporalRuntime.DateTime2;
+export const SmallDateTime = temporalRuntime.SmallDateTime;
+export const DateTimeOffset = temporalRuntime.DateTimeOffset;
