01b88f76b8672b150d644c8f632d5296a76f4f0a
Yirga Hagos <yirga.hagos@avow.tech>
2026-07-21 12:23:32 +0200

    chore: remove internal repository artifacts

--- dist/utils/id.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createId = createId;
-function createId(prefix) {
-    return `${prefix}_${Date.now().toString(36)}_${Math.random().toString(36).slice(2, 10)}`;
-}
-//# sourceMappingURL=id.js.map
